a63b7765222ae8b1f3b72c919ef9e380459f7405
MaNa <139146852+amna33mcc@users.noreply.github.com>
2023-08-28 22:19:34 +0330

    Update geo-update.sh

--- geo-update.sh
+++ geo-update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash2
+#!/bin/bash
 Month=$(date +"%m")
 Year=$(date +"%Y")
 wget https://download.db-ip.com/free/dbip-country-lite-${Year}-${Month}.csv.gz -O /usr/share/xt_geoip/dbip-country-lite.csv.gz
